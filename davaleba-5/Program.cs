e5c2082b36b7d623c66bacc0a650ac1bfa67c6f0
dduuddii <60303873+dduuddii@users.noreply.github.com>
2021-06-20 23:03:53 +0400

    Update Program.cs

--- davaleba-5/Program.cs
+++ davaleba-5/Program.cs
@@ -1,111 +1,31 @@
 using System;
-using System.Collections;
 
-namespace Davaleba_6
+namespace Davaleba_5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MyStack s = new MyStack();
-            s.Add(3);
-            s.Add(5);
-            s.Min();
-            s.Add(2);
-            s.Add(1);
-            s.Min();
-            s.Remove();
-            s.Min();
-            s.Remove();
-            s.Peek();
-        }
-    }
-    public class MyStack
-    {
-        public Stack s;
-        public int min;
-
-        public MyStack()
-        {
-            s = new Stack();
+            int z = 4;
+            Console.WriteLine(countVariants(z));
+            Console.ReadKey();
         }
 
-        public void Min()
+        public static int x(int y) 
         {
-            if (s.Count == 0)
+            if (y <= 1)
             {
-                Console.WriteLine("Stack is empty");
+                return y;
             }
             else 
             {
-                Console.WriteLine("Minimum element in the " + " stack is: " + min);
-            }
-                
-        }
-
-        public void Peek()
-        {
-            if (s.Count == 0)
-            {
-                Console.WriteLine("Stack is empty ");
-                return;
-            }
-
-            int t = (int)s.Peek();
-            Console.Write("Top element is: ");
-
-            if (t < min) 
-            { 
-                Console.WriteLine(min);
-            }
-            else 
-            {
-                Console.WriteLine(t);
+                return x(y - 1) + x(y - 2);
             }
         }
 
-        public void Add(int x)
+        public static int countVariants(int stearsCount) 
         {
-            if (s.Count == 0)
-            {
-                min = x;
-                s.Push(x);
-                Console.WriteLine("Number added: " + x);
-                return;
-            }
-
-            if (x < min)
-            {
-                s.Push(2 * x - min);
-                min = x;
-            }
-
-            else
-                s.Push(x);
-
-            Console.WriteLine("Number added: " + x);
-        }
-
-        public void Remove()
-        {
-            if (s.Count == 0)
-            {
-                Console.WriteLine("Stack is empty");
-                return;
-            }
-
-            Console.Write("Element removed: ");
-            int t = (int)s.Pop();
-
-            if (t < min)
-            {
-                Console.WriteLine(min);
-                min = 2 * min - t;
-            }
-            else 
-            {
-                Console.WriteLine(t);
-            }
+            return x(stearsCount + 1);
         }
     }
 }
