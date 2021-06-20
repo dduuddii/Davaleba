3314a41366494820797e17d1a45935dab6b1c4b1
dduuddii <60303873+dduuddii@users.noreply.github.com>
2021-06-20 22:50:04 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Davaleba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static bool IsPalindrome(string text)
+        {
+            bool IsPalindrome = true;
+            if (text.Length > 0)
+            {
+                for (int i = 0; i < text.Length / 2; i++)
+                {
+                    if (text.Substring(i, 1) != text.Substring(text.Length - (i + 1), 1))
+                    {
+                        IsPalindrome = false;
+                    }
+                }
+            }
+            else
+            {
+                IsPalindrome = false;
+            }
+            return IsPalindrome;
+        }
+    }
+}
